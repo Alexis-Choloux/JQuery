25344b8773c95acb90bf1c59210f41591d661b58
Alexis Choloux <alexis.choloux.pro@gmail.com>
2021-01-11 16:46:24 +0100

    2 - done

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,1 +1,8 @@
+// Jquery est un framework JS fait par des développeurs, reprenant des morceaux de codes régulièrement utilisés en JS, pour les condenser en quelques lignes
+
 console.log('exercice 1');
+
+$(document).ready(function(){
+	console.log('Je peux maintenant écrire du code jQuery');
+});
+// ready permet de lancer ce que comprend la fonction, quand la page est chargée (ready)
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,21 @@
 console.log('exercice 2');
 
+$("#title");
+
+$("*.sidenav");
+
+$("footer");
+
+$("input[placeholder='Search Blog..']");
+
+$(".glyphicon");
+
+$(".label-success");
+
+$("p.intro");
+
+$("img, .glyphicons")
+
+$('.sidenav:first-child');
+
+$("p:odd");
