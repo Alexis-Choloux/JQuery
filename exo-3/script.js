ffe68166dce6a8e2a690cad0a137d0026852e269
Alexis Choloux <alexis.choloux.pro@gmail.com>
2021-01-12 15:25:40 +0100

    ex 4 - almost done

--- exo-3/script.js
+++ exo-3/script.js
@@ -1,1 +1,26 @@
 console.log("exercice 3");
+
+$("h1").text("Alexis Choloux");
+
+$("#tagline").text("Développeur Web - Full Stack");
+
+$("#myNavbar ul:first").remove("");
+
+$(".glyphicon-log-in").addClass("glyphicon-circle-arrow-right").removeClass("glyphicon-log-in");
+
+$("footer p").text("Copyright 2017").css("font-weight", "bold");
+
+$(".col-sm-3 p").each(function(i) {
+    $(this).text('Mon projet ' + (i+1));
+});
+
+$(".col-sm-3 img").each(function(i) {
+    $(this).attr("src", "pony" + (i+1) + ".jpg")
+})
+
+
+let mainImg = "<img src='pony.jpg' style='height: 200px'>";
+
+$(".container-fluid h3").after("<a href='#'><div>" + mainImg + "<p>Ma dernière réalisation</p></div></a>");
+
+$("footer").addClass("intro");
--- exo-4/script.js
+++ exo-4/script.js
@@ -1,1 +1,35 @@
 console.log("exercice 4");
+
+const email = $("input[type='email']").val();
+
+$(".btn-danger").click(function() {
+    alert("Merci nous vous tiendrons informé des différentes offres sur " + email);
+});
+
+$("#myNavbar ul:first li:last").dblclick(function(){
+    $(this).hide();
+});
+
+let article = 1;
+
+$("img").click(function() {
+    $(".navbar-right li:last a").replaceWith("<a href='#'><span class='glyphicon glyphicon-shopping-cart'></span> Cart (" + article++ + ")</a>");
+});
+
+$(".panel").each(function() {
+    $(this).mouseenter(function(){
+        console.log("L'utilisateur regarde : " + $(".panel-footer", this).html());
+    });
+    $(this).mouseleave(function(){
+        console.clear();
+    });
+})
+
+$("input[type='email'").click(function() {
+    console.log("L'utilisateut vient de cliquer sur le formulaire");
+    
+    $(this).on("input", function() {
+    console.clear();
+    console.log("L'utilisateur est en train de taper : " + this.value);
+    });
+})
