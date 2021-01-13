63fcfff7f844e7f525c5dc7c3d8a2c1abd4ea72b
Alexis Choloux <alexis.choloux.pro@gmail.com>
2021-01-13 13:56:45 +0100

    6 - done

--- exo-6/script.js
+++ exo-6/script.js
@@ -1,19 +1,32 @@
 console.log("exercice 6");
 
-$("button").one("click", function (event) {
-
-    event.preventDefault();
-
-    const email = $("#exampleInputEmail1").val();
-    const password = $("#exampleInputPassword1").val();
-
-    if (!email) {
-        $("form").after("<div class='alert alert-danger mt-2'>Le champs \'Email\' est obligatoire !");
-    } 
-    else if (password.length < 6) {
-        $("form").after("<div class='alert alert-danger mt-2'>Votre mot de passe contient " + password.length + " caractères, il doit en compter au moins 6 !");
-    } 
-    else {
-        console.log(email, password);
+function isEmail(email) {
+    let regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!regex.test(email)) {
+        return false;
+    } else {
+        return true;
     }
-})
+}
+
+    $("button").one("click", function (event) {
+
+        event.preventDefault();
+
+        const email = $("#exampleInputEmail1").val();
+        const password = $("#exampleInputPassword1").val();
+
+        if (!email) {
+            $("form").after("<div class='alert alert-danger mt-2'>Le champs \'Email\' est obligatoire !");
+        }
+        else if (isEmail(email) == false) {
+            $("form").after("<div class='alert alert-danger mt-2'>Merci d'insérer un email valide !");
+        }
+        else if (password.length < 6) {
+            $("form").after("<div class='alert alert-danger mt-2'>Votre mot de passe contient " + password.length + " caractères, il doit en compter au moins 6 !");
+        }
+        else {
+            alert('Vous êtes connecté !');
+        }
+    })
+
