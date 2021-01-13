50c2aa3f1c63901a79097eea89f7af757933ebe1
Alexis Choloux <alexis.choloux.pro@gmail.com>
2021-01-13 12:41:17 +0100

    6 - in progresse

--- exo-6/script.js
+++ exo-6/script.js
@@ -1,1 +1,19 @@
 console.log("exercice 6");
+
+$("button").one("click", function (event) {
+
+    event.preventDefault();
+
+    const email = $("#exampleInputEmail1").val();
+    const password = $("#exampleInputPassword1").val();
+
+    if (!email) {
+        $("form").after("<div class='alert alert-danger mt-2'>Le champs \'Email\' est obligatoire !");
+    } 
+    else if (password.length < 6) {
+        $("form").after("<div class='alert alert-danger mt-2'>Votre mot de passe contient " + password.length + " caractères, il doit en compter au moins 6 !");
+    } 
+    else {
+        console.log(email, password);
+    }
+})
